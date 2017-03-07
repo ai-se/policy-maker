fb7f9834876c3b674713794f8673ae7f324f1aff
George Mathew <george.meg91@gmail.com>
2017-03-06 20:21:17 -0500

    Heatmaps and Perplexity

--- doc_parser.py
+++ doc_parser.py
@@ -4,19 +4,22 @@ import os
 sys.path.append(os.path.abspath("."))
 sys.dont_write_bytecode = True
 
-import json
-import requests
 from bs4 import BeautifulSoup
-import re
-import pickle
-import lda
 from collections import OrderedDict
 from sklearn.feature_extraction.text import CountVectorizer, ENGLISH_STOP_WORDS
 from nltk.stem.porter import PorterStemmer
-import numpy as np
 from utils.lib import O
+from sklearn.model_selection import train_test_split
+from utils.lda_extended import log_perplexity as perplexity
+import json
+import requests
+import re
+import pickle
+import lda
+import numpy as np
 import matplotlib.pyplot as plt
 import logging
+import pandas as pd
 
 logging.getLogger('lda').setLevel(logging.ERROR)
 
@@ -38,14 +41,40 @@ IOT_PATH = "files/data/iot.pkl"
 SUS_DEV_PATH = "files/data/sus_dev.pkl"
 
 # LDADE
-TOKEN_PATTERN = re.compile(r"(?u)\b\w\w+\b")
+TOKEN_PATTERN = re.compile(r"(?u)\b[a-zA-Z]{2}[a-zA-Z]+\b")
 ITERATIONS = 100
 ALPHA = None
 BETA = None
 STOP_WORDS = ENGLISH_STOP_WORDS.union(['software', 'engineering'])
-N_TOPICS = 7
+N_TOPICS = 10
 RANDOM_STATE = 1
-
+AGENCY_MAP = {
+  'Transportation Department': 'DOT',
+  'Federal Transit Administration': 'FTA',
+  'Commerce Department': 'DOC',
+  'International Trade Administration': 'ITA',
+  'Economic Development Administration': 'EDA',
+  'National Oceanic and Atmospheric Administration': 'NOAA',
+  'Federal Highway Administration': 'FHWA',
+  'Interior Department': 'DOI',
+  'Fish and Wildlife Service': 'FWS',
+  'Defense Department': 'DOD',
+  'Navy Department': 'USN',
+  'Health and Human Services Department': 'HHS',
+  'Food and Drug Administration': 'FDA',
+  'Environmental Protection Agency': 'EPA',
+  'National Highway Traffic Safety Administration': 'NHTSA',
+  'Children and Families Administration': 'ACF',
+  'National Institute of Standards and Technology': 'NIST',
+  'Housing and Urban Development Department': 'HUD',
+  'Federal Railroad Administration': 'FRA',
+  'Maritime Administration': 'MARAD',
+  'Army Department': 'USA',
+  'National Aeronautics and Space Administration': 'NASA',
+  'National Telecommunications and Information Administration': 'NTIA',
+  'National Technical Information Service': 'NTIS'
+}
+PERMITTED_AGENCIES = ['DOC', 'DOT', 'NASA', 'HHS', 'DOD']
 
 class Document(O):
   """
@@ -197,17 +226,58 @@ def optimal_topics(file_name, to_file, fig_name):
   for topic_count in topics:
     print("TOPICS : ", topic_count)
     docs = Document.load(file_name)
-    graph = Graph(docs)
+    train, test = train_test_split(docs, test_size=0.2, random_state=RANDOM_STATE)
+    graph = Graph(train)
     model, vocabulary = graph.lda(topic_count)
-    topic_scores.append(model.loglikelihood())
+    perplex = perplexity(model, graph, test)
+    topic_scores.append(perplex)
+    # topic_scores.append(model.loglikelihood())
   write_to_file(to_file, topics, topic_scores)
   plot_topics(topics, topic_scores, fig_name)
 
 
 def plot_topics(topics, scores, fig_name):
   plt.plot(topics, scores, 'ro')
+  plt.xlabel('Topics ->')
+  plt.ylabel('Log Perplexity ->')
   plt.savefig(fig_name)
   plt.clf()
+
+
+def make_heatmap(arr, row_labels, column_labels, fig_name):
+  plt.figure(figsize=(4, 3))
+  df = pd.DataFrame(arr, columns=column_labels, index=row_labels)
+  cax = plt.matshow(df, interpolation='nearest', cmap='hot_r')
+  plt.colorbar(cax)
+  plt.xticks(np.arange(len(list(df.columns))), list(df.columns), rotation="vertical")
+  plt.yticks(np.arange(len(list(df.index))), list(df.index))
+  plt.title("Topics to Conference Distribution", y=1.2)
+  plt.savefig(fig_name, bbox_inches='tight')
+  plt.clf()
+
+
+def diversity(documents, fig_name):
+  graph = Graph(documents)
+  model, vocabulary = graph.lda()
+  report_lda(model, vocabulary)
+  heatmap_map = {}
+  valid_agencies = []
+  for agency, documents in graph.agency_map.items():
+    if agency not in AGENCY_MAP or AGENCY_MAP[agency] not in PERMITTED_AGENCIES: continue
+    topics = np.array([0] * model.n_topics)
+    for document in documents:
+      topics = np.add(topics, graph.document_map[document].topic_count)
+    if sum(topics) > 0:
+      heatmap_map[agency] = topics
+      valid_agencies.append(agency)
+  row_labels = ["Topic %1d" % ind for ind in range(model.n_topics)]
+  column_labels = [AGENCY_MAP[agency] for agency in valid_agencies]
+  heatmap_arr = []
+  for agency in sorted(heatmap_map.keys()):
+    tot = sum(heatmap_map[agency])
+    dist = [top / tot for top in heatmap_map[agency]]
+    heatmap_arr.append(dist)
+  make_heatmap(np.transpose(heatmap_arr), row_labels, column_labels, fig_name)
 
 
 def _optimal():
@@ -220,11 +290,12 @@ def _optimal():
 
 
 def _main():
-  docs = Document.load('files/data/sus_dev.pkl')
-  graph = Graph(docs)
-  model, vocabulary = graph.lda()
-  report_lda(model, vocabulary)
+  keyword = "sus_dev"
+  docs = Document.load('files/data/%s.pkl' % keyword)
+  diversity(docs, 'files/results/figs/%s_diversity.png' % keyword)
+  # model, vocabulary = graph.lda()
+  # report_lda(model, vocabulary)
 
 
 if __name__ == "__main__":
-  _optimal()
+  _main()
